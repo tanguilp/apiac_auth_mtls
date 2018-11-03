638a6862d011a135d5ee287067c4fbf5bedf1714
Tangui <29804907+tanguilp@users.noreply.github.com>
2018-11-03 20:43:28 +0300

    Added test

--- test/apisex_auth_mtls_test.exs
+++ test/apisex_auth_mtls_test.exs
@@ -36,12 +36,14 @@ defmodule APISexAuthMTLSTest do
       "/C=RU/ST=SPB/L=SPB/O=APISexAuthBearer/CN=test server certificate",
       template: :root_ca)
 
-    res = Plug.Cowboy.https(SelfSignedValidCert, [],
+    ref = make_ref()
+
+    Plug.Cowboy.https(SelfSignedValidCert, [],
                       port: 8443,
+                      ref: ref,
                       cert: X509.Certificate.to_der(server_ca_cert),
                       cacerts: [X509.Certificate.to_der(peer_root_cert)],
                       key: {:ECPrivateKey, X509.PrivateKey.to_der(server_ca_private_key)},
-                      #fail_if_no_peer_cert: true,
                       verify: :verify_peer)
 
     {:ok, {status, _headers, body}} =
@@ -53,9 +55,56 @@ defmodule APISexAuthMTLSTest do
         key: {:ECPrivateKey, X509.PrivateKey.to_der(peer_private_key)}
       ]],
       [])
+    
+    Plug.Cowboy.shutdown(ref)
 
     assert elem(status, 1) == 200
     assert Poison.decode!(body)["apisex_client"] == "testclient"
     assert Poison.decode!(body)["apisex_authenticator"] == "Elixir.APISexAuthMTLS"
+  end
+
+  test "invalid pki certificate" do
+    peer_root_private_key = X509.PrivateKey.new_ec(:secp256r1)
+    peer_root_cert = X509.Certificate.self_signed(peer_root_private_key,
+      "/C=BZ/ST=MBH/L=Lorient/O=APISexAuthBearer/CN=test root CA peer certificate",
+      template: :root_ca
+    )
+
+    peer_private_key = X509.PrivateKey.new_ec(:secp256r1)
+    peer_cert =
+      peer_private_key
+      |> X509.PublicKey.derive()
+      |> X509.Certificate.new(
+         "/C=BZ/ST=MBH/L=Lorient/O=APISexAuthBearer/CN=invalid DN",
+         peer_root_cert, peer_root_private_key,
+         extensions: [subject_alt_name: X509.Certificate.Extension.subject_alt_name(["localhost"])])
+
+    server_ca_private_key = X509.PrivateKey.new_ec(:secp256r1)
+    server_ca_cert = X509.Certificate.self_signed(server_ca_private_key,
+      "/C=RU/ST=SPB/L=SPB/O=APISexAuthBearer/CN=test server certificate",
+      template: :root_ca)
+
+    ref = make_ref()
+    Plug.Cowboy.https(SelfSignedValidCert, [],
+                      port: 8443,
+                      ref: ref,
+                      cert: X509.Certificate.to_der(server_ca_cert),
+                      cacerts: [X509.Certificate.to_der(peer_root_cert)],
+                      key: {:ECPrivateKey, X509.PrivateKey.to_der(server_ca_private_key)},
+                      verify: :verify_peer)
+
+    {:ok, {status, _headers, _body}} =
+      :httpc.request(:post,
+      {'https://localhost:8443', [], 'application/x-www-form-urlencoded', 'client_id=testclient'},
+      [ssl: [
+        cacerts: [X509.Certificate.to_der(server_ca_cert)],
+        cert: X509.Certificate.to_der(peer_cert),
+        key: {:ECPrivateKey, X509.PrivateKey.to_der(peer_private_key)}
+      ]],
+      [])
+    
+    Plug.Cowboy.shutdown(ref)
+
+    assert elem(status, 1) == 401
   end
 end
